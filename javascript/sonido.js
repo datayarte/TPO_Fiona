3aa65d53b4d86b756ad1b476e90448fbb7498cce
oriana camussoni <camussonioriana@gmail.com>
2023-10-09 09:25:12 -0300

    inicio

--- javascript/sonido.js
+++ javascript/sonido.js
@@ -16,3 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Cambia la imagen cada 2 segundos
+}
+
+showSlides();
