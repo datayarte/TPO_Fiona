06ca2cff17c61b62a1e2cf1ff424798fd62875a0
Marcelo Torres <marcelo@datayarte.com.ar>
2023-10-23 15:33:56 -0300

    Feat: validar form

---
+++ javascript/validarform.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("miFormulario");
+
+    formulario.addEventListener("submit", function (event) {
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
+        const numero = document.getElementById("numero").value;
+
+        if (nombre.trim() === "") {
+            alert("Por favor, ingrese su nombre y apellido.");
+            event.preventDefault();
+        } else if (email.trim() === "") {
+            alert("Por favor, ingrese su correo electrónico.");
+            event.preventDefault();
+        } else if (!validateEmail(email)) {
+            alert("Por favor, ingrese una dirección de correo electrónico válida.");
+            event.preventDefault();
+        } else if (numero.trim() !== "" && !/^\d{10}$/.test(numero)) {
+            alert("Por favor, ingrese un número de teléfono válido (10 dígitos numéricos).");
+            event.preventDefault();
+        }
+    });
+
+    function validateEmail(email) {
+        const re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+});
